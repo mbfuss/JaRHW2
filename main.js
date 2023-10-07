6c8e9b08c6044a4a955ff870c907096140af1407
mbfuss <oldqug@yandex.ru>
2023-10-07 18:53:36 +0400

    HW2

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-// переменная для хранения мат. действия
+// Переменная для хранения мат. действия
+var op;
+var op;
+var op;
+var op;
+var op;
+var op;
+
+
 var op;
 var oneObj = {};
-// функция рассчета
+// Функция рассчета
 function calculate() {
-    // переменная для результата
+    // Переменная для результата
     var result;
 
     // Number - будем приравнивать число число. По умолчанию строка.
@@ -25,20 +33,39 @@ function calculate() {
             result = num1 / num2;
             break;
     }
-    // отправляем результат в html 
+    // Отправляем результат в html 
     document.getElementById("result").innerHTML = result;
 
-    // для передачи перменных в функцию history()
+    // Для передачи перменных в функцию history()
     oneObj.num1 = num1;
     oneObj.num2 = num2;
     oneObj.result = result;
 }
 
 function history(){
-var historyElement = document.createElement('div'); // создаем отдельный блочных элемент
+var historyElement = document.createElement('div'); // Создаем отдельный блочных элемент
      num1 = oneObj.num1;
      num2 = oneObj.num2;
      result = oneObj.result;
       historyElement.innerHTML = num1 + ' ' + op + ' ' + num2 + ' = ' + result;
-      document.getElementById('history').appendChild(historyElement); // передача в <div id="history"></div> из historyElement
+      document.getElementById('history').appendChild(historyElement); // Передача в <div id="history"></div> из historyElement
     }
+
+
+// В зависимости от положения курсора будет вводится button
+var activeInput = null; // Переменная для хранения активного <input>
+
+        function setActiveInput(inputNumber) {
+            activeInput = document.getElementById(`num${inputNumber}`);
+        }
+
+        function appendToActiveInput(value) {
+            if (activeInput) {
+                // Добавляем значение кнопки в конец активного <input>
+                activeInput.value += value;
+            }
+        }
+function clearInput() {
+            document.getElementById('num1','num2').value = '';
+            document.getElementById('num2').value = '';
+        }
